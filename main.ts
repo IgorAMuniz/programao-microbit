0d7e98568335a941795d99618fe55f7283d24e67
IgorAMuniz <113916957+IgorAMuniz@users.noreply.github.com>
2022-10-03 11:40:30 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,7 +1,47 @@
-input.onPinPressed(TouchPin.P1, function () {
-    basic.showString("LET'S PLAY")
-})
 let p2 = 0
+let p1 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showString("LET'S PLAY")
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.clearScreen()
+    basic.pause(randint(1000, 10000))
+    basic.showLeds(`
+        # # . # #
+        . # . # .
+        . . # . .
+        . # . # .
+        # # . # #
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    p2 += 1
+    basic.showLeds(`
+        . . . # #
+        . . . # .
+        . . # . .
+        . . . # .
+        . . . # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P1, function () {
+    p1 += 1
+    basic.showLeds(`
+        # # . . .
+        . # . . .
+        . . # . .
+        . # . . .
+        # # . . .
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+})
 basic.forever(function () {
-	
+    p1 = 0
+    p2 = 0
 })
